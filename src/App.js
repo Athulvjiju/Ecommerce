93fab3ed720d11a7616d72570fa35ab8bf438a8b
Athulvjiju <athulathuttan39@gmail.com>
2023-05-09 15:27:34 +0530

    ecommerce

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Headers';
+import Footer from './components/Headers/Footer';
+import Home from './modules/Home';
+import {Routes,Route} from 'react-router-dom'
+import Product from './components/Product';
+import Products from './modules/Products';
+import CategoryProducts from './modules/CategoryProducts';
+import Cart from './modules/Cart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/products/:id' element={<Product/>}/>
+        <Route path='/products' element={<Products/>}/>
+        <Route path='/categories/:name' element={<CategoryProducts/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='*' element={<div className='text-center '> 404 Not Found</div>}/>
+        </Routes>
+      <Footer/>
+      </div>
   );
 }
 
---
+++ src/modules/Home/index.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import FeatureCard from '../../components/Headers/FeatureCard'
+import Hero from '../../components/Headers/Hero'
+import ProductCard from '../../components/Headers/ProductCard'
+import Products from '../../components/Headers/ProductCard'
+import Stats from '../../components/Headers/StatCard'
+import Categories from '../../components/Categories'
+
+const Home = () => {
+    const [products,setProducts]=useState([])
+    useEffect(() => {
+        const fetchProducts = async () => {
+           const response=await fetch('https://fakestoreapi.com/products?limit=12')
+           const data=await response.json()
+           console.log(data);
+           setProducts(data)
+        }
+        fetchProducts()
+    },[])
+
+
+    return (
+        <>
+            <Hero />
+            <Categories/>
+            <div className="flex flex-col text-center w-full">
+                <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">PRODUCTS</h2>
+                <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">ALL PRODUCTS</h1>
+            </div>
+            {
+                products.length > 0 ?
+                <ProductCard products={products}/>
+                :
+                <div> Loading...</div>
+            }
+           
+            <Products />
+        
+            <Stats />
+        </>
+    )
+}
+
+export default Home
